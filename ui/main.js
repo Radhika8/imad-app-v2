71bb919c2575dbfb055e7bbf88bf15fdf90b2662
Radhika8 <radhikameena84@gmail.com>
2017-03-24 00:07:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,3 @@
-/*-console.log('Loaded!');
-// change the text of main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-//Move the Image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-    
-}
-img.onclick = function() {
-   // img.style.marginLeft = '100px';
-   
-   var interval = setInterval(moveRight,50);
-    
-};
-*/
 
 
 var button = document.getElementById('counter');
