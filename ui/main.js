60de60cae3620e559f734bad2e724e99bb558ca2
Radhika8 <radhikameena84@gmail.com>
2017-03-24 10:25:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ button.onclick = function(){
     request.onreadystatechange = function(){
         
         if(request.readyState === XMLHttpRequest.DONE){
-            //Take some acction
+            //Take some action
             if(request.status === 200){
                 
                 var counter = request.responseText;
@@ -30,8 +30,32 @@ button.onclick = function(){
     };
     
     //Make the Request
-    
     request.open('GET', 'http://radhika8.imad.hasura-app.io/counter',true);
     request.send(null);
 
 };
+
+  //Submit name
+  
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick = function(){
+      //Make a request to the server and send name
+      //Capture a list of names and render it as a list
+      var names = ['name1','name2','name3','name4'];
+      
+      var list = '';
+      for(var i=0;i<names.length;i++)
+         {
+          
+          list+= '<li>'+names[i]+'</li>';
+          }
+      var ul = document.getElementById('namelist');
+      ul.innerHTML = list;
+      
+      
+  };
+  
+  
+  
