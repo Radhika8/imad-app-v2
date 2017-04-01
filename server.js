a298de451ad4aa8d887c75bd247fe8bc18a9cf99
Radhika8 <radhikameena84@gmail.com>
2017-04-01 09:49:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/test-db',function(req,res){
                  }
             else
                {
-                   res.send(JSON.stringify(result.rows));
+                   res.send(JSON.stringify(result));
                }
             
          });
