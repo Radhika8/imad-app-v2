75555e8ba06a031ac58c28ce812b123a90f62c4b
Radhika8 <radhikameena84@gmail.com>
2017-04-06 22:19:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(morgan('combined'));
 function hash (input, salt){
    
     var hashed  = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-     return hashed.toString('hex');
+     return["[pbkdf2","10000" ,salt,hashed.toString('hex')].join('$');
     
     
 }
